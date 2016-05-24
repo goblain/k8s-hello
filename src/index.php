67c668953733d4b54cfd7028a1017135cf2031db
Radek Pieczonka <goblin@pentex.pl>
2016-05-24 15:44:52 +0200

    fix h2 closure typo

--- src/index.php
+++ src/index.php
@@ -1,4 +1,4 @@
-<h2>Suggested random names :<h2>
+<h2>Suggested random names :</h2>
 <ul>
 <?php
 
@@ -12,7 +12,7 @@ for($i=0; $i < pow($n, $k); $i++) {
 
 for($i=0; $i<10; $i++) {
   $resp = json_decode(file_get_contents('http://namegen'));
-  echo('<li>Name: '.$resp->name.' - Host: '.$resp->hostname.' - Version: '.$resp->version);
+  echo('<li>Name: <b>'.$resp->name.'</b> - Host: <b>'.$resp->hostname.'</b> - Version: <b>'.$resp->version.'</b>');
 }
 ?>
 </ul>
